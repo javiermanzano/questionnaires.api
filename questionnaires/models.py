b177178384249f08580884b8fd7b3d5dabfda394
Javier Manzano <javier.manzano22@gmail.com>
2021-02-21 21:10:17 +0100

    added models

--- questionnaires/models.py
+++ questionnaires/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+  first_name = models.CharField(max_length=254)
+  last_name = models.CharField(max_length=254)
+  email = models.EmailField(max_length=254)
+
+class CollectedData(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  created = models.DateTimeField('date')
