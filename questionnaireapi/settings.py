68afac4e7480c4ec266a86be058411cee0d0c283
Javier Manzano <javier.manzano22@gmail.com>
2021-02-22 21:00:28 +0100

    postgres database

--- questionnaireapi/settings.py
+++ questionnaireapi/settings.py
@@ -91,9 +91,14 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'questionnaire',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
+
 }
 
 
