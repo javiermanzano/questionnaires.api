29a796e7343aa30791e3b142047b61849d5616c1
Javier Manzano <javier.manzano22@gmail.com>
2021-02-22 18:29:41 +0100

    autogenerate created at

--- questionnaires/models.py
+++ questionnaires/models.py
@@ -5,7 +5,7 @@ class CollectedData(models.Model):
   last_name = models.CharField(max_length=254)
   email = models.EmailField(max_length=254)
   providers = models.CharField(max_length=254)
-  created = models.DateTimeField('date')
+  created = models.DateTimeField(auto_now_add=True)
 
   def __str__(self):
     return self.providers
